a264d523df77a9c0db8e39ba1a9832f93761a15a
Cassandra Claire <cassandra_villaruel@yahoo.com>
2017-10-03 14:25:18 -0500

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# FriendFinder
+# Friend Finder
+
+Have you, a pokemon, ever wondered which other pokemon you are truly compatible with?
+Friend Finder is a Pokemon compatibility application. This application takes in results from you and other users, and then compares the results with the answers from other users (and pre-made users).
+
+Technologies Used:
+Node.js, Express.js, body-parser, path, heroku, Bootstrap, Chosen, JQuery
+
+Front End: 
+The Home and Survey pages were created using HTML and Bootstrap. In the Survey page, there is some Javascript/JQuery that stores the results from the users. An AJAX POST request is then made to send data (as objects) to the server. 
+
+Back End: 
+The back end is composed of a node/express server that stores results from the users as objects within a friends array. This is also where logic was implemented to compare data between users, and where the html routing was created. 
+
+Live site: 
+
+## How the Website Works Works 
+
+![Home Page](.images/1.png)
+
+In the homepage, you are prompted to go to the survey with a button. Once the button is clicked, you will be taken to the survey page.
+
+![Survey](.images/2.png)
+
+Once you are in the survey page, you will have to input your name and a picture. Afterwards, you have to fill out 10 statements with answers ranging from a scale of 1 - 5. 1 means you strongly disagree with a statement, and 5 means you strongly agree with a statement.
+
+If you do not fill out all of the fields, a validation process will alert you to ensure that all fields are filled out. 
+
+![Result](.images/3.png)
+
+After you have submited your answers, a modal will pop up displaying your best match. 
+If you would like to see your results posted to the friends api, you can click on the link on the bottom of the page.
+
+![API](.images/4.png)
+
+
+- - -
+
+Copyright (c) 2017 Cassandra Villaruel
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,37 +13,59 @@ module.exports = function(app){
 		res.json(friends);
 	}); // Close: Gets information from friends API
 
+	// Open: Posts results to friends api
 	app.post('/api/friends', function(req, res){
 
+		// variables needed for logic
+		// this best friend variable holds the info for the user's current best friend
+		// match diff is a high arbitrary number used as a placeholder
 		var bestFriend = {
 			name: "",
 			image: "",
-			matchDif: 1000
+			matchDif: 100
 		};
+
+		// getting user information, as an object, from the html
 		var userInfo = req.body;
+		// storing the user's score into a variable
 		var userScores = userInfo.scores;
 
+		// initialize total difference
 		var totalDiff = 0;
 
-		for(var i = 0; i < [friends].length; i++){
+		// Open: Loop to go through every single friend on the list
+		for(var i = 0; i < friends.length; i++){
             
             totalDiff = 0;
 
-		
-			for(var j = 0; j < friends[i].scores[j]; j++){
+			// Open: Loop through the current friend in the loops' scores
+			for(var j = 0; j < friends[j].scores[j]; j++){
 				
+				// logic to find the difference of the users scores and the friends scores
 				totalDiff += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+
+				// results added to the console
+				console.log("Friend Name: " + friends[i].name + " || Total Difference: " + totalDiff);
 				
+				// if the total difference is less than or equal to the current best friend
+				// then that pokemon's info will be updated as the new best friend
 				if (totalDiff <= bestFriend.matchDif){
 					bestFriend.name = friends[i].name;
 					bestFriend.image = friends[i].photo;
 					bestFriend.matchDif = totalDiff;
 				}
-			}
-		}
+			} // Close: Loop through all of the stored friends scores
 
+
+		} // Close: Loop to go through every single friend on the list
+		
+		// spacing out results on command line
+		console.log("");
+
+		// push user info into friends array to store them for future use
 		friends.push(userInfo);
  
+		// best friend object can be used for on the front end now
 		res.json(bestFriend);
 	});
 };
